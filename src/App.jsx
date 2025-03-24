1c55b5a335dc917eceef98f5c3eb9996bc1f4e63
neeljoe <joe.neel@gmail.com>
2025-03-24 11:28:09 +0530

    secon commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,17 @@ const App = () => {
   return (
     <div>
       <Navbar />
+      <h1>App component her</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis dicta at harum aspernatur doloribus, quo quia perspiciatis quos deserunt qui quibusdam sit. Error incidunt maxime quasi ratione debitis, deserunt dicta.
+      Natus vero explicabo, provident eveniet, quis molestias esse perspiciatis temporibus ab et aut modi sapiente. Aut voluptatem, deserunt neque nemo necessitatibus magnam, ducimus obcaecati, modi aspernatur repudiandae natus dolore totam!
+      Velit eos autem molestiae, ad doloremque facere deleniti debitis vero, neque corrupti tempore aliquid amet modi, veritatis veniam illum obcaecati! Expedita sunt at sed officiis eligendi quae odit, dolor delectus?
+      At qui, accusamus dolore placeat, explicabo odio nostrum sunt quaerat sit laudantium nulla sint sed enim assumenda fuga iure vero quidem nemo. Consequuntur id delectus ratione omnis error qui tenetur.
+      Voluptate, exercitationem suscipit consectetur nobis mollitia neque officiis sapiente doloribus praesentium, veritatis fuga illum sint officia nisi cum dolor ipsam ad obcaecati labore pariatur laboriosam. Eveniet, magni similique? Ratione, sapiente.
+      Dolor rem nobis libero, voluptate totam exercitationem id, ab vel nulla voluptates dolore alias repudiandae dignissimos quis. Perspiciatis praesentium accusantium culpa totam, laborum quae eum, nihil facilis rerum vero reiciendis.
+      Enim, aut facilis! Tenetur labore veniam id velit, fugit voluptate ratione? Quas itaque molestias optio ea temporibus laborum recusandae tempore maiores facere, corrupti odio, animi cumque est dolor mollitia expedita!
+      Earum odio fugiat blanditiis ipsam! Laboriosam et reiciendis quaerat. Amet, eligendi, nobis ut unde, quis ab debitis dicta aliquam veniam fuga esse eum quidem fugit iste recusandae! Vitae, voluptate et!
+      Repellat ducimus deleniti temporibus quasi expedita. Quas impedit, animi ipsam quo iure voluptatum sint sunt ea velit quis voluptatem sapiente quod unde magni ipsum ut adipisci, omnis aliquam neque labore.
+      Unde reprehenderit dolor suscipit soluta delectus corporis vel totam voluptatibus odit cum natus magni veritatis iusto provident labore tenetur quis aliquam expedita, accusantium sunt magnam impedit cupiditate, animi alias? Nihil.</p>
 
     </div>
   )
