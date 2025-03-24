631bb064df72ece3c6f418db38f12110ce2931b2
Shelton Joseph <joe.neel@gmail.com>
2025-03-24 22:26:31 +0530

    single post added back

--- src/routes/SinglePost.jsx
+++ src/routes/SinglePost.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const SinglePost = () => {
   return (
-    <div>SinglePost</div>
+    <div>
+      <h1>Single post here</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt dolorem quos inventore voluptate, saepe distinctio dolores nisi quidem aut sequi vero dolor placeat esse. Suscipit iure repudiandae sunt possimus dolor.
+      Quam itaque illo, possimus dolorum deleniti temporibus, asperiores magnam sint harum nesciunt quod cum aut modi nihil dolorem, repellendus distinctio ducimus nemo iure laborum! Labore minus laboriosam necessitatibus doloribus velit!
+      Obcaecati ducimus, blanditiis commodi optio harum at corporis ab dolore, cum ullam, quod vel delectus quisquam officia hic earum perspiciatis illum adipisci. Voluptates nesciunt placeat sit maxime consequatur dolores vel.
+      Repellendus necessitatibus deleniti, nam eligendi assumenda placeat possimus blanditiis sint et voluptatem natus pariatur, voluptatibus, eum accusantium! Iusto molestiae minima architecto, reiciendis explicabo quae, ut corporis nobis ea rem modi.
+      Maiores pariatur temporibus ullam nisi blanditiis at minus praesentium natus sed ex reprehenderit dolores esse dolore rerum doloremque asperiores tempora, facilis aperiam consectetur cum. Recusandae nihil perferendis obcaecati unde quo!
+      Veniam saepe sint assumenda odit earum magni, exercitationem accusamus vel officia debitis corrupti adipisci quibusdam nam ea, laudantium, quasi dolores? Obcaecati repellendus possimus, molestias qui praesentium laboriosam eos voluptatem consectetur.
+      Rem iusto sunt asperiores ipsa expedita doloremque dicta dolor qui ipsam ipsum perferendis magnam minus, quis rerum soluta nemo reiciendis odio enim, voluptatibus amet facilis! Labore aut assumenda ut saepe.
+      Voluptatum quidem dolore eligendi cumque ab nulla! Aliquid voluptatibus dolorem porro ullam eum similique esse placeat architecto sunt magni? Qui quo architecto ducimus id. Eum voluptatum vel numquam laborum obcaecati?
+      Maiores facere fugit quidem impedit error autem adipisci eum ratione iste beatae molestiae id officia, repellat voluptates soluta sunt aspernatur dolorum blanditiis fuga rem architecto distinctio dolores? Beatae, minus sequi?
+      Ad ab dolor laborum, molestias eius nesciunt deleniti, voluptates sint velit quam error. Voluptatem reiciendis est repellat, veritatis doloremque distinctio illum odio recusandae voluptates id libero in soluta voluptatibus eum.
+      Reiciendis velit voluptatem quisquam voluptates animi, recusandae veritatis magnam blanditiis consequuntur, est vitae quasi accusamus quis eveniet ratione perferendis labore ducimus, tempore ullam iusto. Veniam pariatur quis temporibus impedit perspiciatis.
+      Odio ullam deserunt, in a quasi, veritatis consequatur corrupti esse aut temporibus repudiandae nihil at voluptates minima placeat aliquid! Quam odio tenetur nihil debitis labore perspiciatis dicta aliquid recusandae ab.</p>
+    </div>
   )
 }
 
