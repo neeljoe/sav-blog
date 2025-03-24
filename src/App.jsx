33794021f76fa1dde425b1a651ce6ddaf201cb71
neeljoe <joe.neel@gmail.com>
2025-03-24 16:31:56 +0530

    router commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import Navbar from './components/Navbar'
 const App = () => {
   return (
     <div className='px-4 md:px-8 lg:px-15 lx:px-32 2xl:px-64'>
-      <Navbar />
-      <h1>App component her</h1>
+      {/* <Navbar />
+      <h1>App component her</h1> */}
      
     </div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,56 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import MainLayout from './layouts/MainLayout.jsx'
+import HomePage from './routes/HomePage.jsx'
+import LoginPage from './routes/LoginPage.jsx'
+import RegisterPage from './routes/RegisterPage.jsx'
+import PostListPage from './routes/PostListPage.jsx'
+import SinglePost from './routes/SinglePost.jsx'
+import Write from './routes/Write.jsx'
 
+
+
+const router = createBrowserRouter([
+ {
+  element: <MainLayout />,
+  children: [
+    {
+      path: '/',
+      element: <HomePage />,
+    },
+    {
+      path: '/posts',
+      element: <PostListPage />,
+    },
+    {
+      path: '/:slug',
+      element: <SinglePost />,
+    },
+    {
+      path: '/write',
+      element: <Write />,
+    },
+    {
+      path:'/login',
+      element:<LoginPage/>,
+    },
+    {
+      path: '/register',
+      element: <RegisterPage />,
+    },
+
+  ]
+ }
+],
+{
+  basename: '/sav-blog',
+})
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}
+    >
+    </RouterProvider>
   </StrictMode>,
 )
