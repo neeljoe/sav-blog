f43ad6168b3920d63378d52d68c08357f02c626e
neeljoe <joe.neel@gmail.com>
2025-03-25 17:08:11 +0530

    search bar

--- src/components/MainCategories.jsx
+++ src/components/MainCategories.jsx
@@ -17,9 +17,11 @@ const MainCategories = () => {
           </div>
           <div className="divider divider-horizontal"></div>
         {/* search */}
-        <div>
-            Search
-        </div>
+        <label className="input rounded-full border-0 outline-0">
+          <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2.5" fill="none" stroke="currentColor"><circle cx="11" cy="11" r="8"></circle><path d="m21 21-4.3-4.3"></path></g></svg>
+          <input type="search" className="grow " placeholder="Search" />
+          
+        </label>
     
     </div>
   )
