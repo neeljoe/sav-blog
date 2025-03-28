9087bed939c7aa502e60f974a63d4c7a5c3b34d0
neeljoe <joe.neel@gmail.com>
2025-03-28 14:35:41 +0530

    quill failed

--- src/routes/Write.jsx
+++ src/routes/Write.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import { useUser } from '@clerk/clerk-react'
 
 const Write = () => {
-  return (
-    <div>Write</div>
+  const {isLoaded, isSignedIn} = useUser()
+  if (!isLoaded) { 
+    return <div> Loading ...</div>
+  }
+  if (isLoaded && !isSignedIn) {
+    return <div> Please sign in to write a post</div>
+  }
+
+    return (
+    <div>
+      <h1>Create a New Post</h1>
+      <form>
+        <button>Add a Cover Image</button>
+        <input type="text" placeholder="My Awesome Story" />
+        <div>
+          <label htmlFor="">Choose a category</label>
+          <select name="cat" id="">
+            <option value="general">General</option>
+            <option value="fashion">Fashion</option>
+            <option value="lifestyle">LifeStyle</option>
+            <option value="health">Health</option>
+            <option value="career">Career</option>
+            <option value="travel">Travel</option>
+          </select>
+        </div>
+        <textarea name="desc" placeholder='A Short Description' />
+      </form>
+    </div>
   )
 }
 
