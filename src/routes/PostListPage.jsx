85df87f396ef566cca9ce41d60a80e701d4c3e45
Shelton Joseph <joe.neel@gmail.com>
2025-03-29 22:13:30 +0530

    sidemenu extended

--- src/routes/PostListPage.jsx
+++ src/routes/PostListPage.jsx
@@ -3,7 +3,9 @@ import TravelList from '../components/TravelList'
 import { Link } from 'react-router-dom'
 import SideMenu from '../components/SideMenu'
 
+
 const PostListPage = () => {
+  const [open, setOpen] = React.useState(false)
   return (
     <div className='my-5'>
      <div className="breadcrumbs text-xl mb-6">
@@ -12,12 +14,14 @@ const PostListPage = () => {
         <li className='text-blue-500'>Travel Blogs</li>
       </ul>
     </div>
-      <div className='flex gap-8'>
+    <button onClick={() => setOpen((prev) => !prev)} className='bg-rose-500 text-white text-sm px-4 py-2 rounded-2xl hover:bg-rose-600 cursor-pointer md:hidden mb-4'>
+      {open ? "close" : "Filter & Search"}</button>
+      <div className='flex flex-col-reverse md:flex-row gap-8'>
         <div className=''>
           <TravelList />
         </div>
-        <div className=''>
-          <SideMenu />
+        <div className={`${open ? "block" : "hidden"} md:block`}>
+          <SideMenu />                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
         </div>
 
       </div>
